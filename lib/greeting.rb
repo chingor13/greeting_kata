2ae75e19044173b1da49b18eb5ab32ce5b24b6e8
Jeff Ching <ching.jeff@gmail.com>
2015-09-01 19:40:02 -0700

    fix requirement 4

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,11 +1,16 @@
 require "greeting/version"
+require "active_support/all"
 
 module Greeting
   def self.greet(name)
     name ||= "my friend"
 
-    if name == name.upcase
-      return "HELLO #{name}!"
+    if name.is_a?(Array)
+      return "Hello, #{name.to_sentence}."
+    else
+      if name == name.upcase
+        return "HELLO #{name}!"
+      end
     end
 
     "Hello, #{name}"
